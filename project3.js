d07a89711aa21ab933bacd2bfd42445fa496c5b4
Sean Graney <sgraney@hawk.iit.edu>
2020-11-24 14:38:51 -0600

    adding initial map
    map features include:
    Centered on the Bean
    Initial centered marker
    marker added anywhere on click
    Button that finds your location if you allow google to do so

---
+++ project3.js
@@ -0,0 +1,83 @@
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+let myMap, infoWindow;
+
+function init(){
+  // alert('it worked')
+  var el = document.getElementById('canvas');
+  var myLocation = new google.maps.LatLng(41.88263161464457, -87.62330426490517 );
+  var mapOptions = {
+    center: myLocation,
+    zoom: 20,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  }
+  var myMap = new google.maps.Map(el, mapOptions)
+
+  var iconBase = 'https://maps.google.com/mapfiles/kml/pushpin/';
+  var marker = new google.maps.Marker({
+   position: myLocation,
+   icon: iconBase + 'red-pushpin.png',
+   myMap,
+   title: "Hello World!",
+   draggable: true,
+  });
+  myMap.addListener("click", (e) => {
+    placeMarkerAndPanTo(e.latLng, myMap);
+  });
+  marker.setMap(myMap);
+
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  myMap.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(myMap);
+          myMap.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(myMap);
+}
+
+function placeMarkerAndPanTo(latLng, myMap) {
+  var iconBase = 'https://maps.google.com/mapfiles/kml/pushpin/';
+  new google.maps.Marker({
+    position: latLng,
+    icon: iconBase + "grn-pushpin.png",
+    map: myMap,
+  });
+  myMap.panTo(latLng);
+}
+google.maps.event.addDomListener(window, 'load', init);
